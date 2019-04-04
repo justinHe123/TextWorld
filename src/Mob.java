acb5a076f2e665cd14283d009d12f8868e9e7608
justinHe123 <justinme100@gmail.com>
2019-04-04 11:14:47 -0700

    added incomplete player finding method to finish later

--- src/Mob.java
+++ src/Mob.java
@@ -16,11 +16,38 @@ public abstract class Mob {
 
     public Graph.Node findPlayer(Player p){
         List<Graph.Node> visited = new ArrayList<>();
+        List<Graph.Node> unvisitedNeighbors = new ArrayList<>();
+        if (currentNode == p.getCurrentRoom()) return currentNode;
+        visited.add(currentNode);
+
         for (Graph.Node node : currentNode.getNeighbors()){
-            if (node == p.getCurrentRoom()) return node;
+            if (!visited.contains(node)) {
+                if (node == p.getCurrentRoom()) return node;
+            } else {
+                visited.add(node);
+                unvisitedNeighbors.add(node);
+            }
         }
-        return null;
+
+        return findPlayer(p, visited, unvisitedNeighbors);
     }
+
+    public Graph.Node findPlayer(Player p, List<Graph.Node> visited, List<Graph.Node> toVisit){
+        List<Graph.Node> unvisitedNeighbors = new ArrayList<>();
+        for (Graph.Node nA : toVisit){
+            for (Graph.Node nB : nA.getNeighbors()){
+                if (!visited.contains(nB)){
+                    if (nB == p.getCurrentRoom()) return nB;
+                } else {
+                    visited.add(nB);
+                    unvisitedNeighbors.add(nB);
+                }
+            }
+        }
+        if (unvisitedNeighbors.size() == 0) return null;
+        else return findPlayer(p, visited, unvisitedNeighbors);
+    }
+    //how do we track the path?
 
     public void moveRandom(){
         System.out.println("This method works");
